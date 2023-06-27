27df10971b1c7329330d972f5d26121c9fc1c56d
Kiera Cox <kiera.cox18@gmail.com>
2023-06-26 21:08:08 -0700

    setting up server and crud files

--- model.py
+++ model.py
@@ -29,3 +29,18 @@ class Appointment(db.Model):
     def __repr__(self):
         return f"Appointment id={self.id} for user: {self.user_id} on: {self.date}"
 
+def connect_to_db(flask_app, db_uri="postgresql:///jobs", echo=False):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = False
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    connect_to_db(app)
